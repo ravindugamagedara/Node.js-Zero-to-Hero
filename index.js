16273ea502c007565c1e95e265d620751d256c5d
Ravindu Gamagedara <ravindugamagedara2001@gmail.com>
2026-09-23 02:38:17 +0530

    learned how to create data and read and get by Id

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-require('dotenv').config();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Node.js Server! gamma');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running smoothly!");
 });
+
+//create data
+
+const bookSchema = new mongoose.Schema({
+  bookName: { type: String, required: true },
+  bookStock: { type: Number, required: true },
+});
+
+bookModel = mongoose.model("Book", bookSchema);
+
+app.post("/books", async (req, res) => {
+  try {
+    const newBook = await bookModel.create(req.body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//read data
+
+app.get("/books", async (req, res) => {
+  try {
+    const bookList = await bookModel.find();
+    res.status(200).send(bookList);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//get by id
+
+app.get("/books/:id" , async (req , res) => {
+  try {
+    const {id} = req.params;
+    const book = await bookModel.findById(id);
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
@@ -14,6 +57,11 @@ app.listen(PORT, () => {
 
 const connectionString = process.env.CONNECT_STRING;
 
-mongoose.connect(connectionString)
-.then(()=> {console.log("Connected to MongoDB")})
-.catch((error) => {console.error("Error connecting to MongoDB:", error)});
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
